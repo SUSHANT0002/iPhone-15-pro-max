c5536b8806415f59c6403bde6182752bc179d71d
SUSHANT0002 <sushant.mahadwad@gmail.com>
2024-09-26 16:27:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Highlights from './Components/Highlights';
+import Model from './Components/Model';
+import Features from './Components/Features';
+import HowItWorks from './Components/HowItWorks';
+import Footer from './Components/Footer';
+const App = () => {
+  return (
+    <>
+      <main className='bg-black'>
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
+        <Features />
+        <HowItWorks />
+        <Footer />
+      </main>
+    </>
+  )
+}
+
+export default App;
